1c20ae102f4450b679be2c2b00c6019e1b00b493
Nicolas R <atoomic@cpan.org>
2019-06-13 09:21:51 -0600

    slide1

--- slide-1.pl
+++ slide-1.pl
@@ -1,5 +1,32 @@
 #!perl
 
+package my::test::File;
+
+use Test2::Bundle::Extended;
+
+# load it before your code
+use Overload::FileCheck q(:all);
+
+is My::Code::do_something(), undef, "-e is unmocked";
+
+mock_file_check(
+    '-e' => sub {
+        my ($file_or_fh) = @_;
+        return CHECK_IS_TRUE if $file_or_fh eq '/any/path';
+        return FALLBACK_TO_REAL_OP;
+    }
+);
+
+is My::Code::do_something(), "Something", "-e is now mocked";
+
+unmock_file_check('-e');
+is My::Code::do_something(), undef, "-e is unmocked";
+
+done_testing;
+exit;
+
+# ------------------------------------------------------------
+
 package My::Code;
 
 sub do_something {
@@ -7,18 +34,3 @@ sub do_something {
 
     return "Something";
 }
-
-### .
-package my::test::File;
-
-use Overload::FileCheck q(:all);
-
-mock_file_check(
-    '-e' => sub {
-        my ($file_or_fh) = @_;
-
-        return CHECK_IS_TRUE if $file_or_fh eq '/any/path';
-        return FALLBACK_TO_REAL_OP;
-    }
-);
-
