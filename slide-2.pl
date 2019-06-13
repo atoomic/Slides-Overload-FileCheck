1a853a8cf7f69d97eaa4cad43fe0ece57b7f5b5c
Nicolas R <atoomic@cpan.org>
2019-06-13 09:25:31 -0600

    slide-2.pl

--- slide-2.pl
+++ slide-2.pl
@@ -1,24 +1,32 @@
 #!perl
 
-package My::Code;
-
-sub do_something {
-    return unless -e q[/any/path];
-
-    return "This is a Dir"  if -d q[/any/path];
-    return "This is a File" if -f q[/any/path];
-
-    return "No Ideas come back later";
-}
-
-### .
 package my::test::File;
+
+use Test2::Bundle::Extended;
 
 use Overload::FileCheck q(:all);
 
+ok ! -e '/any/path';
+ok ! -d '/any/path';
+ok ! -f '/any/path';
+
 mock_file_check( '-e' => \&dash_check );
+
+ok -e '/any/path';
+ok ! -d '/any/path';
+ok ! -f '/any/path';
+
 mock_file_check( '-d' => \&dash_check );
+
+ok -e '/any/path';
+ok -d '/any/path';
+ok ! -f '/any/path';
+
 mock_file_check( '-f' => \&dash_check );
+
+ok -e '/any/path';
+ok -d '/any/path';
+ok -f '/any/path';
 
 sub dash_check {
     my ($file_or_fh) = @_;
@@ -26,3 +34,5 @@ sub dash_check {
     return CHECK_IS_TRUE if $file_or_fh eq '/any/path';
     return FALLBACK_TO_REAL_OP;
 }
+
+done_testing;
